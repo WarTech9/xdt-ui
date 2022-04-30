3eda90d5f22dd69c0756c79358cf269393435764
Wartech <john.warmann@gmail.com>
2022-04-30 02:13:11 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'xd-token-ui',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
