682f3bcc616d3d665076aabaefce13f3d3dfe31c
Wartech <john.warmann@gmail.com>
2022-05-01 15:12:43 -0400

    always dark mode

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -10,7 +10,9 @@ import { WalletProviderService } from '../services/wallet-provider.service';
 })
 export class TabsPage implements OnInit, OnDestroy {
   isCorrectNetwork = true;
+  account: any;
   networkSubscription?: Subscription;
+  accountSubscription?: Subscription;
   constructor(
     private wallet: WalletProviderService,
     private zone: NgZone,
@@ -29,14 +31,19 @@ export class TabsPage implements OnInit, OnDestroy {
   }
 
   setupListeners() {
-    this.networkSubscription = this.wallet.networkSubject.subscribe(chainId => {
+    this.networkSubscription = this.wallet.networkSubject.subscribe(async chainId => {
       console.log('got chainId = ', chainId);
+      await this.wallet.getAccounts();
       if (chainId) {
         this.zone.run(() => {
           this.isCorrectNetwork = chainId.toString(16).toLowerCase() === environment.chainId.toLocaleLowerCase();
           console.log(`Networks: ${chainId} <=> ${environment.chainId}`);
         });
       }
+    });
+
+    this.accountSubscription = this.wallet.accountSubject.subscribe(account => {
+      this.account = account;
     });
   }
 }
